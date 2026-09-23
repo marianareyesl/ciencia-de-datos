3e276430c5c4efc2b50aa2cf18b79ca4200ea515
MARIANA REYES LOBATO <reyeslobatomariana0@gmail.com>
2026-09-23 18:44:12 +0000

    hola

--- data.py
+++ data.py
@@ -2,5 +2,7 @@ import pandas as pd
 import streamlit as st
 
 names_link = "https://raw.githubusercontent.com/marianareyesl/generos-de-musica/refs/heads/main/music_genre.csv"
+names_dataframe = pd.read_csv(names_link)
 
-names_data = pd.read_csv(names_link)
+st.title ("streamlit and pandas")
+st.dataframe(names_data)
--- hello-dash.py
+++ hello-dash.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
 st.title("hola")
-dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
+dataframe = pd.read_csv("https://raw.githubusercontent.com/marianareyesl/generos-de-musica/refs/heads/main/music_genre.csv")
 st.dataframe(dataframe)
 st.write("by marianareyesl")
