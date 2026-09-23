92825dbf001b8289c22c232a6427fad35cac3269
MARIANA REYES LOBATO <reyeslobatomariana0@gmail.com>
2026-09-23 18:30:51 +0000

    hola

--- data.py
+++ data.py
@@ -1,8 +1,6 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 names_link = "https://raw.githubusercontent.com/marianareyesl/generos-de-musica/refs/heads/main/music_genre.csv"
-names_data = pd.read_csv(names_link)
 
-st.title("streamlit and pandas")
-st.data(names_data)
+names_data = pd.read_csv(names_link)
